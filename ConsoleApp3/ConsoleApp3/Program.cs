8fb2bff361f771451bd0c315d1017de1fcc0e944
Adam Fahey <afahey87@gmail.com>
2017-10-06 15:30:39 -0400

    Nunit and Test Class
    Learned about Nunit and Test Class

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;   
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.thetestroom.com/webapp");
+            driver.FindElement(By.Id("contact_link")).Click();
+            driver.FindElement(By.TagName("input")).SendKeys("Jon Snow");
+            driver.FindElement(By.Name("address_field")).SendKeys("123 elm street");
+
+
+            IWebElement element = driver.FindElement(By.XPath("id('cdona' )"));
+            element.Click();
+
+
+            IList<IWebElement> links = driver.FindElements(By.TagName("a"));
+
+            foreach (IWebElement link in links)
+                if (link.Text.Equals("ABOUT"))
+                {
+                    link.Click();
+                    break;
+
+                }
+
+           // driver.Close();
+           // driver.Quit();
+
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/WebDriverTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ConsoleApp3
+{
+    [TestFixture]
+    class WebDriverTest
+    {
+        [Test]
+        public void SampleTest()  // Do not put ; here. I know you want too. :)
+
+
+        {
+
+            int a = 10;
+            int b = 10;
+
+            Assert.AreEqual(a, b);
+
+        
+        }
+
+        [Test]
+
+        public void sampletestwebdriver()
+        {
+
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.thetestroom.com/webapp");
+
+            Assert.True(driver.Url.Contains("helloworld"));                      // Verifying that the url contains the "webapp"
+            driver.Close();
+
+
+        }
+
+
+
+    }
+
+}
